c59f72c5e6f3761e075f2a8a182d4cb9c7c466e0
Guadalupe Gonzalez <31290160+Guadag12@users.noreply.github.com>
2020-05-12 15:28:56 -0300

    Update and rename sd to Desafios.R

---
+++ Clase 04/Ejercicios_clase/Desafios.R
@@ -0,0 +1,26 @@
+#1. activamos tidyverse
+
+#2. cargamos el dataset de "recipeData.csv"
+
+# Desafio 1: Agrupar la cantidad de cervezas por estilo, filtrarlas 
+#y dejar solo aquellos estilos que tienen mas de 1000 tipos de cerveza.
+# Hacer un grafico de barras que muestre la cantidad de cervezas que hay
+#de cada tipo
+
+# Desafio 2: Tenemos que filtrar el dataset para dejar solo los 12 estilos de
+#cerveza que nos quedaron en el 1° grafico.
+# Una vez hecho esto, lo agrupamos por Style y armarmos 2 nuevas columnas
+#con el promedio de Amargor y Graduacion Alcoholica.
+# Por ?ltimo, armamos un grafico de puntos para que nuestros 12 estilos 
+#de cerveza se acomoden en una tabla que tenga Amargor en una absisa y 
+#Graduacion Alcoholica en la otra.
+
+# Desafio 3: 
+#Hacer un scatterplot con hasta 25  casos con las variables ABV e IBU de las cervezas
+#tipo American IPA, Blonde Ale e Imperial IPA	donde se distinga que tipo es cada una.
+#Agregar etiquetas a cada punto, a los ejes y colocar t?tulo.
+
+#Desafío 4
+#Desafío abierto! Armar un gráfico con las variables que uno quiera
+#Agregando la paleta viridis e incorporando t?tulos a ambos ejes
+#No olvidar poner el t?tulo
